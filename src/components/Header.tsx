696580e95223903ffb93f99059989e70ab682a39
Abraham Jimenez <q6br73e4@duck.com>
2025-01-08 21:28:47 -0700

    Fetch store name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,32 @@ import {
   ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
 
+const data = await fetch(
+  "https://jimen971.myshopify.com/admin/api/2025-01/graphql.json",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": process.env.ADMIN_ACCESS_TOKEN as string,
+    },
+    body: JSON.stringify({
+      query: `{
+              shop {
+                  name
+              }
+          }`,
+    }),
+  },
+);
+
+const {
+  data: {
+    shop: { name },
+  },
+} = await data.json();
+
+console.log(name);
+
 const Header = () => {
   return (
     <div className={"flex justify-between"}>
@@ -11,7 +37,7 @@ const Header = () => {
         <div>
           <Bars3Icon className={"size-6"} />
         </div>
-        <p>Joyeria La Perla</p>
+        <p>{name}</p>
       </div>
 
       <div className={"flex gap-2"}>
