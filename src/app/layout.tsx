4564136763a540e8b99ecfa3180e124c1f1fc765
Abraham Jimenez <j8wehlbn@duck.com>
2025-02-10 14:33:40 -0700

    Add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import "@mantine/core/styles.css";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
           </footer>
         </MantineProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
