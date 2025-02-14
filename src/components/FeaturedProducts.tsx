44f617106c51ed4d2052205f78e0068acfccfde9
Abraham Jimenez <j8wehlbn@duck.com>
2025-02-14 10:36:50 -0700

    Fixed incorrect || logic to ??

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -19,7 +19,7 @@ const FeaturedProducts = ({ data }: { data: ProductData }) => {
                   key={productEdge.node.images.edges[0].node.id}
                   src={productEdge.node.images.edges[0].node.url}
                   alt={
-                    productEdge.node.images.edges[0].node.altText ||
+                    productEdge.node.images.edges[0].node.altText ??
                     productEdge.node.title
                   }
                   className="transition-opacity duration-300 ease-in-out opacity-100"
@@ -37,7 +37,7 @@ const FeaturedProducts = ({ data }: { data: ProductData }) => {
                       key={productEdge.node.images.edges[1].node.id}
                       src={productEdge.node.images.edges[1].node.url}
                       alt={
-                        productEdge.node.images.edges[1].node.altText ||
+                        productEdge.node.images.edges[1].node.altText ??
                         productEdge.node.title
                       }
                       className="transition-opacity duration-300 ease-in-out opacity-0 group-hover:opacity-100 absolute top-0 left-0"
