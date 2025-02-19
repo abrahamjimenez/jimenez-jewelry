5a42ec020e31829605c05b3ee2c1a34f0a23377d
Abraham Jimenez <j8wehlbn@duck.com>
2025-02-19 14:48:07 -0700

    Fix image src

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 const Page = () => {
   return (
@@ -13,7 +12,7 @@ const Page = () => {
 
       <hr/>
 
-      <img src={"https://abrahamjimenez.myshopify.com/cdn/shop/files/ER-0020.jpg?v=1739554751&width=300"} alt={"image"} width={300} height={300} />
+      <img src={"https://jimenezjewelry.com/cdn/shop/files/IMG_0102.heic?v=1739766547&width=2890"} alt={"image"} width={300} height={300} />
       <p>14K 3C Fancy Hollow Earrings with Lever Back</p>
       <p>$300</p>
       <button>+1 | 1 | -1</button>
