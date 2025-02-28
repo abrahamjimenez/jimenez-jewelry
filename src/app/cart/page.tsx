d4f423bb8eab4186426a81f7c390ac0bdbd86c9d
Abraham Jimenez <j8wehlbn@duck.com>
2025-02-27 17:42:03 -0700

    Add todo

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -117,24 +117,10 @@ const Page = () => {
     fetchCartData();
   }, [cartId]);
 
+  // todo removeOneItemQuery
+
   return (
     <div>
-      {/*<h1>Your cart</h1>
-      <div className="flex justify-between">
-        <p>Product</p>
-        <p>Total</p>
-      </div>
-      <hr />
-      <img
-        src="https://jimenezjewelry.com/cdn/shop/files/IMG_0102.heic?v=1739766547&width=2890"
-        alt="image"
-        width={300}
-        height={300}
-      />
-      <p>14K 3C Fancy Hollow Earrings with Lever Back</p>
-      <p>$300</p>
-      <button>+1 | 1 | -1</button>*/}
-
       {cartData && (
         <table>
           <caption>Your cart</caption>
