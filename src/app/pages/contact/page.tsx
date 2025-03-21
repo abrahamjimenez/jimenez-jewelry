17df0092548ff96d27a47417948fd906681ac202
Abraham Jimenez <j8wehlbn@duck.com>
2025-03-21 16:28:32 -0600

    #34 Add form using mantine

---
+++ src/app/pages/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactForm from "@/components/ContactForm";
+
+const Page = () => {
+    return (
+        <div className={"pt-10"}>
+            <h2 className={"text-2xl"}>Contact Form</h2>
+
+            <ContactForm />
+        </div>
+    );
+};
+
+export default Page;
