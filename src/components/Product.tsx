e4b8c55ddc92f563a7c287991fe7f07e0468f85c
Abraham Jimenez <j8wehlbn@duck.com>
2025-03-23 18:21:54 -0600

    #2 Improved code
    - Add max quantity
    - Add number to left in stock
    - Moved structure for improved styling

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -94,50 +94,51 @@ const Product = ({ data }: { data: ProductData }) => {
 
   return (
     <div className={"p-4 sm:px-6 lg:p-8"}>
-      <div className={"md:grid md:grid-cols-2 gap-10 mx-auto"}>
+      <div className={"md:grid md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-10 mx-auto"}>
         <ImageCarousel images={imageUrls} />
         <div className={"pt-6 flex flex-col gap-4"}>
           <h2 className={"text-2xl md:text-3xl"}>{data.title}</h2>
-          <p className={"font-bold text-sm md:text-lg"}>
+          <p className={"text-sm md:text-lg"}>
             ${parseFloat(data.variants.nodes[0]?.price.amount || "0").toFixed(2)} USD
           </p>
-          <p className={"text-xs text-gray-500 md:hidden"}>Quantity</p>
+          <p className={"text-xs text-gray-500"}>Quantity</p>
+
+          <Paper p="md" withBorder className={"flex justify-center min-w-1/2 w-2/3"} classNames={classes}>
+            <Button variant={"transparent"} onClick={() => handlersRef.current?.decrement()}>
+              <MinusIcon className="size-6" />
+            </Button>
+
+            <NumberInput
+              classNames={classes}
+              handlersRef={handlersRef}
+              min={1}
+              max={data.variants.nodes[0]?.quantityAvailable}
+              value={quantity}
+              onChange={(value) => {
+                if (typeof value === "number") {
+                  setQuantity(value);
+                }
+              }}
+              defaultValue={1}
+              hideControls
+            />
+            <Button variant={"transparent"} onClick={() => handlersRef.current?.increment()}>
+              <PlusIcon className="size-6" />
+            </Button>
+          </Paper>
+
+          <p className={"text-gray-500 text-xs md:text-sm lg:text-lg md:text-gray-600 pt-4"}>
+            {data.variants.nodes[0]?.quantityAvailable > 0 ? `${data.variants.nodes[0]?.quantityAvailable} left in stock` : "Sold out"}
+          </p>
+          <Button
+            disabled={data.variants.nodes[0]?.quantityAvailable === 0}
+            onClick={handleAddToCart}
+            fullWidth
+          >
+            Add to Cart
+          </Button>
         </div>
       </div>
-
-      <Paper p="md" withBorder className={"flex justify-center"} classNames={classes}>
-        <Button variant={"transparent"} onClick={() => handlersRef.current?.decrement()}>
-          <MinusIcon className="size-6" />
-        </Button>
-
-        <NumberInput
-          classNames={classes}
-          handlersRef={handlersRef}
-          min={1}
-          value={quantity}
-          onChange={(value) => {
-            if (typeof value === "number") {
-              setQuantity(value);
-            }
-          }}
-          defaultValue={1}
-          hideControls
-        />
-        <Button variant={"transparent"} onClick={() => handlersRef.current?.increment()}>
-          <PlusIcon className="size-6" />
-        </Button>
-      </Paper>
-
-      <p className={"text-gray-500 text-xs md:text-sm lg:text-lg md:text-gray-600 pt-4"}>
-        {data.variants.nodes[0]?.quantityAvailable > 0 ? "In Stock" : "Sold Out"}
-      </p>
-      <Button
-        disabled={data.variants.nodes[0]?.quantityAvailable === 0}
-        onClick={handleAddToCart}
-        fullWidth
-      >
-        Add to Cart
-      </Button>
     </div>
   );
 };
