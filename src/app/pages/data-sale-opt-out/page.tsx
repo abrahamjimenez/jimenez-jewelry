3ab5d2e5ce00de40d79254b4bc29c94227baafab
Abraham Jimenez <j8wehlbn@duck.com>
2025-04-01 12:41:05 -0600

    #27 Extract html with cheerio

--- src/app/pages/data-sale-opt-out/page.tsx
+++ src/app/pages/data-sale-opt-out/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { fetchShopifyData } from "@/utils/shopify";
+import * as cheerio from 'cheerio';
+
+const extractText = (html: string) => {
+  const $ = cheerio.load(html);
+  $("link, meta, script, div").remove();
+  return $('body').text().trim() || $.root().text().trim(); // Get text
+};
+
 
 const Page = async () => {
   const DoNotSharePersonalInformationQuery = `query DoNotShareMyPersonalInformation {
@@ -11,7 +19,12 @@ const Page = async () => {
   const data = await fetchShopifyData(DoNotSharePersonalInformationQuery)
   console.log(data);
 
-  return <div>Do not sell my personal information</div>;
+  const cleanedBody = extractText(data.pageByHandle.body);
+
+  return <div>
+    <p>Do not sell my personal information</p>
+    <p>{cleanedBody}</p>
+  </div>;
 };
 
 export default Page;
