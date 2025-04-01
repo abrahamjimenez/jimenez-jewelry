1c64d0163157255123f62a1ded817e7b8d869812
Abraham Jimenez <j8wehlbn@duck.com>
2025-03-31 19:26:45 -0600

    #27 Add contact information page & update h1 styles

--- src/app/policies/contact-information/page.tsx
+++ src/app/policies/contact-information/page.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { fetchAdminShopifyData } from "@/utils/shopify";
 
-const Page = () => {
+interface ContactInformation {
+  shop: {
+    name: string
+    contactEmail: string
+    billingAddress: {
+      country: string
+  }
+  }
+}
+
+const Page = async () => {
+  const contactInformationQuery = `query ContactInformation {
+    shop {
+      name
+        contactEmail
+        billingAddress {
+          country
+        }
+    }
+  }`
+
+  const data: ContactInformation = await fetchAdminShopifyData(contactInformationQuery)
+  console.log(data);
+
   return (
-    <div>
-      Contact Information
-    </div>
+    <>
+      <h1 className={"text-center pb-6"}>Contact Information</h1>
+      <div className={"flex justify-center"}>
+        <div className={"flex flex-col gap-4 justify-self-center"}>
+          <p>Store Name: {data.shop.name}</p>
+          <p>Email: {data.shop.contactEmail}</p>
+          <p>Country: {data.shop.billingAddress.country}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
