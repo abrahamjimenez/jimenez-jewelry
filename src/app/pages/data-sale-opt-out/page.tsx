8fd4c9c433e95604f4d9e017b4dfe1c3c38f8bde
Abraham Jimenez <j8wehlbn@duck.com>
2025-04-01 13:07:48 -0600

    #27 Prettier

--- src/app/pages/data-sale-opt-out/page.tsx
+++ src/app/pages/data-sale-opt-out/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { fetchShopifyData } from "@/utils/shopify";
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
 const extractText = (html: string) => {
   const $ = cheerio.load(html);
@@ -8,36 +8,40 @@ const extractText = (html: string) => {
 
   // Replace div with p
   $("div").each(function () {
-    const text  = $(this).text().trim()
+    const text = $(this).text().trim();
     if (text) {
-      $(this).replaceWith(`<p>${text}</p>`)
+      $(this).replaceWith(`<p>${text}</p>`);
     } else {
-      $(this).remove()
+      $(this).remove();
     }
-  })
+  });
 
-  return $.html("body").trim()
+  return $.html("body").trim();
 };
-
 
 const Page = async () => {
   const DoNotSharePersonalInformationQuery = `query DoNotShareMyPersonalInformation {
     pageByHandle(handle: "data-sale-opt-out") {
       body
     }
-  }`
+  }`;
 
-  const data = await fetchShopifyData(DoNotSharePersonalInformationQuery)
+  const data = await fetchShopifyData(DoNotSharePersonalInformationQuery);
   console.log(data);
 
   const cleanedBody = extractText(data.pageByHandle.body);
 
-  return <div className={"flex flex-col justify-center items-center"}>
-    <div  className={"max-w-screen-sm"}>
-      <h1>Do not sell or share my personal information</h1>
-      <div className={"flex flex-col gap-4 leading-8"} dangerouslySetInnerHTML={{__html: cleanedBody}} />
+  return (
+    <div className={"flex flex-col justify-center items-center"}>
+      <div className={"max-w-screen-sm"}>
+        <h1>Do not sell or share my personal information</h1>
+        <div
+          className={"flex flex-col gap-4 leading-8"}
+          dangerouslySetInnerHTML={{ __html: cleanedBody }}
+        />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Page;
