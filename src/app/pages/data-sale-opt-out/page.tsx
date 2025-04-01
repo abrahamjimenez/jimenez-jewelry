df04f67d0afde677b0da9b524ed8a35673441bba
Abraham Jimenez <j8wehlbn@duck.com>
2025-04-01 12:32:51 -0600

    #27 Fetch data for data-sale-opt-out page

--- src/app/pages/data-sale-opt-out/page.tsx
+++ src/app/pages/data-sale-opt-out/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { fetchShopifyData } from "@/utils/shopify";
 
-const Page = () => {
+const Page = async () => {
+  const DoNotSharePersonalInformationQuery = `query DoNotShareMyPersonalInformation {
+    pageByHandle(handle: "data-sale-opt-out") {
+      body
+    }
+  }`
+
+  const data = await fetchShopifyData(DoNotSharePersonalInformationQuery)
+  console.log(data);
+
   return <div>Do not sell my personal information</div>;
 };
 
