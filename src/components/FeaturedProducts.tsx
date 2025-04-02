83d72936e4559cfe0a5ddb19f622bcf737f6fb1f
Abraham Jimenez <j8wehlbn@duck.com>
2025-04-02 15:55:38 -0600

    #46 Test LCP

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -36,6 +36,8 @@ const FeaturedProducts = ({ data }: { data: ProductData }) => {
                 src={primaryImage?.url}
                 alt={primaryImage?.altText ?? `${title}-${i + 1}`}
                 priority={i === 0}
+                quality={50}
+                loading={"eager"}
                 className={"hover-primary-image"}
               />
 
@@ -44,6 +46,7 @@ const FeaturedProducts = ({ data }: { data: ProductData }) => {
                 height={360}
                 src={secondaryImage?.url}
                 alt={""}
+                loading={"lazy"}
                 className={"hover-secondary-image"}
               />
 
